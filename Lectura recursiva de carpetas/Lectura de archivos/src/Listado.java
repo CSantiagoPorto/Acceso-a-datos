e1db16b52d9690fd852e0de8ea98dea502d82740
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2024-10-14 18:24:19 +0200

    Funcionando sin problemas

--- Lectura recursiva de carpetas/Lectura de archivos/src/Listado.java
+++ Lectura recursiva de carpetas/Lectura de archivos/src/Listado.java
@@ -15,19 +15,21 @@ public class Listado {
         //creo el objeto con la ruta para pasárselo al método
         //Este objeto creado con la ruta se la pasará al método listadoFicheros cuando lo llame en la siguiente línea
         listadoFicheros(ruta);
-
     }
-public static void listadoFicheros(File rutaFichero) {
+public static void listadoFicheros(File rutaFichero) {//Static porque no tiene sentido que no sea de la clase
         //Creamos el método pasándole el archivo rutaFichero que contiene la ruta
     //Ahora sólo necesito que liste si es una carpeta. Por lo tanto necesito la condición
-     if(rutaFichero.isDirectory()){
-         for(File item: rutaFichero.listFiles()){
-             //Como rutaFichero es un objeto File puedo usar sus métodos y listFiles lista las carpetas
-             System.out.println(item.getName());//imprimimos nombre
-             if(rutaFichero.isDirectory()){
-                 //Usamos el isDirectory como antes para comprobar si hay una capreta dentro de otra
-                 listadoFicheros(item);
-                 //Esto debería permitir llamar a la propia función para cada item que cumpla la condición
+     if( rutaFichero !=null && rutaFichero.isDirectory()){
+         if(rutaFichero.listFiles()!=null) {//Cuando las carpetas dentro de la carpeta están vacías peta, es null
+             //por lo tanto si compruebo elresultado de rutaFichero.listFiles antes no casca
+             for (File item : rutaFichero.listFiles()) {
+                 //Como rutaFichero es un objeto File puedo usar sus métodos y listFiles lista las carpetas
+                 System.out.println(item.getName());//imprimimos nombre
+                 if (rutaFichero.isDirectory()) {
+                     //Usamos el isDirectory como antes para comprobar si hay una capreta dentro de otra
+                     listadoFicheros(item);
+                     //Esto debería permitir llamar a la propia función para cada item que cumpla la condición
+                 }
              }
          }
 
